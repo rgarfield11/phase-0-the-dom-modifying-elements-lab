c3a3690628553cd6b1b12897acb0d42f6267c644
Ross <rossgarfield@gmail.com>
2022-05-02 16:35:52 -0500

    commit tree aa60363b20baef5244e98662ac1eece3048d6c08

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.body.remove('main');
+
+const newHeader = document.createElement('h1');
+// document.head.append(newHeader);
+newHeader.id = 'victory';
+newHeader.textContent = 'ROSS is the champion';
+
+
